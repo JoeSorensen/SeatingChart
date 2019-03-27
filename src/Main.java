5b392ede775c3e5d830f31cac1f4f4341d4d0f3a
Joe Sorensen <Joseph.md.Sorensen@icloud.com>
2019-03-27 08:25:59 -0600

    Added vcs.xml and reformatted code

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
         List<Student> studentList = new ArrayList<Student>();
 
-        studentList.add(new Student("Karen", 3) );
-        studentList.add(new Student("Liz", 1) );
-        studentList.add(new Student("Paul", 4) );
-        studentList.add(new Student("Lester", 1) );
-        studentList.add(new Student("Henry", 5) );
-        studentList.add(new Student("Renee", 9) );
-        studentList.add(new Student("Glen", 2) );
-        studentList.add(new Student("Fran", 6) );
-        studentList.add(new Student("David", 1) );
-        studentList.add(new Student("Danny", 3) );
+        studentList.add(new Student("Karen", 3));
+        studentList.add(new Student("Liz", 1));
+        studentList.add(new Student("Paul", 4));
+        studentList.add(new Student("Lester", 1));
+        studentList.add(new Student("Henry", 5));
+        studentList.add(new Student("Renee", 9));
+        studentList.add(new Student("Glen", 2));
+        studentList.add(new Student("Fran", 6));
+        studentList.add(new Student("David", 1));
+        studentList.add(new Student("Danny", 3));
 
         SeatingChart sc = new SeatingChart(studentList, 3, 4);
 
@@ -26,7 +24,8 @@ public class Main
         System.out.println();
 
         System.out.println("Testing for part B");
-        System.out.println("Students removed = " + sc.removeAbsentStudents(4) );;
+        System.out.println("Students removed = " + sc.removeAbsentStudents(4));
+        ;
         sc.printSeatingChart();
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,21 +1,17 @@
-public class Student
-{
+public class Student {
     private String name;
     private int absenceCount;
 
-    public Student(String name, int absenceCount)
-    {
+    public Student(String name, int absenceCount) {
         this.name = name;
         this.absenceCount = absenceCount;
     }
 
-    public String getName()
-    {
+    public String getName() {
         return name;
     }
 
-    public int getAbsenceCount()
-    {
+    public int getAbsenceCount() {
         return absenceCount;
     }
 
